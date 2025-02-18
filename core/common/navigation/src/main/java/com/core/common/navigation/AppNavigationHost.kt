f6a9ff289875d7d9e12ef00352736a35d7262933
Radhakrishna NP <radhakrishna.np@robosoftin.com>
2025-02-18 13:50:47 +0530

    fix unused import

--- core/common/navigation/src/main/java/com/core/common/navigation/AppNavigationHost.kt
+++ core/common/navigation/src/main/java/com/core/common/navigation/AppNavigationHost.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import com.feature.onboarding.ui.main.navigation.OnboardScreenRoute
 import com.feature.onboarding.ui.main.navigation.onboardScreen
 
